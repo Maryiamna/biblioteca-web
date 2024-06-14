d2c02b541684cfce06258cc66da1a586f173622a
maryiamna <mariana.marcondesfc@gmail.com>
2024-06-14 09:16:23 -0300

    Adicionado tela de usuários

--- app.js
+++ app.js
@@ -10,18 +10,18 @@ const port = 3000;
 //criando a conexão com o banco
 const db = mysql.createConnection({
   host: 'localhost',
-  user:'root',
+  user: 'root',
   password: '',
-  database:'biblioteca'
+  database: 'biblioteca'
 });
 
 //conectando com o banco
 db.connect((error) => {
-if(error){
-  console.error('Erro ao conectar ao MySQL:', error)
-}else{
-  console.log("Conectado ao MySQL!")
-}
+  if (error) {
+    console.error('Erro ao conectar ao MySQL:', error)
+  } else {
+    console.log("Conectado ao MySQL!")
+  }
 });
 
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -38,47 +38,47 @@ app.get(['/', '/home'], (req, res) => {
 
 app.get('/acervo', (req, res) => {
   db.query('select a.nome as autor, l.titulo, l.ISBN, l.ano_publicacao from livro l join autor a on l.id_autor = a.id_autor;', (error, results) => {
-    if(error) {
+    if (error) {
       console.log('Erro ao recuperar os livros', error)
     } else {
-      res.render('acervo', {livros:results})
+      res.render('acervo', { livros: results })
     }
   });
 });
 
-app.get('/pesquisarLivros', (req, res)=>{
+app.get('/pesquisarLivros', (req, res) => {
   const pesquisa = req.query.pesquisa;
   console.log(pesquisa)
-  db.query('select a.nome as autor, l.titulo, l.ISBN, l.ano_publicacao from livro l join autor a on l.id_autor = a.id_autor where l.titulo like ? or a.nome like ?', [`%${pesquisa}%`, `%${pesquisa}%`], (error, results)=>{
-    if (error){
+  db.query('select a.nome as autor, l.titulo, l.ISBN, l.ano_publicacao from livro l join autor a on l.id_autor = a.id_autor where l.titulo like ? or a.nome like ?', [`%${pesquisa}%`, `%${pesquisa}%`], (error, results) => {
+    if (error) {
       console.log('Ocorreu um erro ao realizar o filtro')
     } else {
-      res.render('acervo', {livros:results})
+      res.render('acervo', { livros: results })
     }
   });
 });
 
 const carregarAutores = (callback) => {
-  db.query('select * from autor order by nome', (error, results)=>{
-    if (error){
+  db.query('select * from autor order by nome', (error, results) => {
+    if (error) {
       console.log('Erro ao carregar autores', error);
-    }else {
+    } else {
       const autores = results.map(result => result)
       callback(null, autores);
     }
   });
 }
 
-app.get('/livro', (req, res)=>{
+app.get('/livro', (req, res) => {
   const ISBN = req.query.ISBN;
   console.log(ISBN)
-  carregarAutores((error, listaAutores)=>{
-    db.query('Select * from livro where ISBN = ?', [ISBN], (error, results)=>{
-      if (error){
+  carregarAutores((error, listaAutores) => {
+    db.query('Select * from livro where ISBN = ?', [ISBN], (error, results) => {
+      if (error) {
         console.log('Erro ao buscar o livro com ISBN', ISBN)
-      }else {
-        if(results.length > 0){
-          res.render('livro', {autores: listaAutores, livro: results[0]});
+      } else {
+        if (results.length > 0) {
+          res.render('livro', { autores: listaAutores, livro: results[0] });
         } else {
           console.log('Livro não encontrado')
         }
@@ -87,7 +87,7 @@ app.get('/livro', (req, res)=>{
   })
 });
 // get = query, post = body
-app.post('/editarLivro', (req,res)=>{
+app.post('/editarLivro', (req, res) => {
   const ISBN = parseInt(req.body.inputISBN);
   const id_autor = parseInt(req.body.inputAutor);
   const titulo = req.body.inputTitulo;
@@ -95,19 +95,106 @@ app.post('/editarLivro', (req,res)=>{
   const genero = req.body.inputGenero;
   const resumo = req.body.textResumo;
 
-  db.query('update livro set ISBN = ?, titulo = ?, id_autor = ?, ano_publicacao = ?, genero = ?, resumo = ? where ISBN = ?', [ISBN, titulo, id_autor, ano_publicacao, genero, resumo, ISBN], (error, results)=>{
-    if (error){
+  db.query('update livro set ISBN = ?, titulo = ?, id_autor = ?, ano_publicacao = ?, genero = ?, resumo = ? where ISBN = ?', [ISBN, titulo, id_autor, ano_publicacao, genero, resumo, ISBN], (error, results) => {
+    if (error) {
       console.log('Erro ao editar livro')
-    }else {
+    } else {
       res.redirect('/acervo')
     }
   })
 });
 
-app.post('excluirLivro/:ISBN',(req, res)=>{
+app.post('excluirLivro/:ISBN', (req, res) => {
   const ISBN = parseInt(req.params.ISBN)
   console.log(ISBN)
 })
+
+
+app.get(['/usuario'], (req, res) => {
+  db.query('select count(e.id_emprestimo) as qtd_emprestimo, u.id_usuario, u.nome , u.email from usuario u left join emprestimo e on u.id_usuario = e.id_usuario group by u.id_usuario;', (error, results) => {
+    if (error) {
+      console.log('Houve um erro ao recuperar os usuários')
+    } else {
+      res.render('usuario', { usuarios: results })
+    }
+  })
+})
+
+app.get('/pesquisarUsuarios', (req, res) => {
+  const pesquisa = req.query.pesquisa;
+  console.log(pesquisa)
+  db.query('select count(e.id_emprestimo) as qtd_emprestimo, u.id_usuario, u.nome , u.email from usuario u left join emprestimo e on u.id_usuario = e.id_usuario where nome like ? or email like ? group by u.id_usuario; ', [`%${pesquisa}%`, `%${pesquisa}%`], (error, results) => {
+    if (error) {
+      console.log('Ocorreu um erro ao realizar o filtro')
+    } else {
+      res.render('usuario', { usuarios: results })
+    }
+  });
+});
+
+
+app.get('/infoUsuario', (req, res) => {
+  const id_usuario = req.query.id_usuario;
+  console.log(id_usuario)
+
+  db.query('select * from usuario where id_usuario = ?', [id_usuario], (error, results) => {
+    if (error) {
+      console.log('Error ao buscar usuário por ID', id_usuario)
+    } else {
+      if (results.length > 0) {
+        res.render('infoUsuario', { info_usuario: results[0] });
+      } else {
+        console.log('usuario não encontrado.')
+      }
+    }
+  })
+})
+
+
+app.post('/editarUsuario', (req, res) => {
+  const id_usuario = parseInt(req.body.inputUsuario)
+  const nome = req.body.inputNome
+  const email = req.body.inputEmail
+  const senha = parseInt(req.body.inputSenha)
+  db.query('update usuario set id_usuario = ?, nome = ?, email = ?, senha = ? where id_usuario = ?', [id_usuario, nome, email, senha, id_usuario], (error, results) => {
+    if (error) {
+      console.log('Erro ao editar usuário.')
+    } else {
+      res.redirect('/usuario')
+    }
+  })
+})
+
+
+app.get('/cadastrarUsuario', (req, res) => {
+  res.render('cadastrarUsuario');
+})
+
+app.post('/criarUsuario', (req, res) => {
+  const nome = req.body.inputNome
+  const email = req.body.inputEmail
+  const senha = parseInt(req.body.inputSenha)
+  db.query('insert into usuario (nome, email, senha) values (?, ?, ?)', [nome, email, senha], (error, results) => {
+    if (error) {
+      console.log('Erro ao cadastrar usuário.')
+    } else {
+      res.redirect('/usuario')
+    }
+  })
+})
+
+
+app.post('/excluirUsuario/:id_usuario', (req, res) => {
+  const id_usuario = parseInt(req.params.id_usuario)
+  console.log(id_usuario)
+  db.query('delete from usuario where id_usuario = ?', [id_usuario], (error, results) => {
+    if (error) {
+      console.log('Erro ao excluir o usuário')
+    } else {
+      res.redirect('/usuario')
+    }
+  })
+});
 
 
 app.listen(port, () => {
